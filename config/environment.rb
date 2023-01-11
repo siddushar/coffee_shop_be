6c348cc3434c2a3ccdc3493804d3fd143df3520a
siddushar <siddushar@gmail.com>
2023-01-11 18:14:50 +0530

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,8 +6,8 @@ Rails.application.initialize!
 
 
 # ActionMailer::Base.smtp_settings = {
-#   :user_name => 'siddusharr@gmail.com', # This is the string literal 'apikey', NOT the ID of your API key
-#   :password => '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', # This is the secret sendgrid API key which was issued during API key creation
+#   :user_name => 'test@gmail.com', # This is the string literal 'apikey', NOT the ID of your API key
+#   :password => 'SG.asdssa._IAOnosuGwvWQAzYQIiCwdxYnqVlJ46RkT6qYOFUkBc', # This is the secret sendgrid API key which was issued during API key creation
 #   :domain => 'localhost:5000',
 #   :address => 'smtp.sendgrid.net',
 #   :port => 587,
